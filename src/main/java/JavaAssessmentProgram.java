f9fa8e6cd4926cc1e49e594c385a8bc640f7e73e
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2022-03-24 19:52:38 +0100

    second commit

---
+++ src/main/java/JavaAssessmentProgram.java
@@ -0,0 +1,71 @@
+import java.lang.reflect.Array;
+
+public class JavaAssessmentProgram {
+    private static Console console = new Console();
+    public static void main(String... args){
+        console.Q1();
+        console.Q2();
+        console.Q3();
+    }
+
+    private static class Console{
+
+        public Console(){
+
+        }
+        //Q1 Solution
+        private void Q1(){
+            int first = 2;
+            String second = "4";
+            int result =  first + Integer.parseInt(second); //The adjusted Line 3 as stated by the question
+            writeLine(result);
+        }
+
+        //Q2 Solution
+        private void Q2(){
+            double x = 47.62;
+            String y = "951";
+            String z = "61x!";
+
+            int X = 0;
+            int Y = 0;
+            int Z = 0;
+
+            //TRY TO CONVERT
+            try{
+                  X = (int) x;
+                  Y = Integer.parseInt(y);
+                  Z = Integer.parseInt(z);
+            }catch (Exception e){
+
+            }finally {
+                writeLine(X);
+                writeLine(Y);
+                writeLine(Z);
+            }
+        }
+
+        //Q3 Solution
+        private void Q3(){
+            var elements = new int[]{25,4,33,87,2,99,60,19,81,45,10,19,72};
+            writeLine(calculateSumOfArrayElement(elements));
+            writeLine(calculateAverageOfArrayElement(elements));
+        }
+
+        private int calculateSumOfArrayElement(int[] elements){
+            var total = 0;
+            for(var element : elements){
+                total = total + element;
+            }
+            return total;
+        }
+
+        private int calculateAverageOfArrayElement(int[] elements){
+            return calculateSumOfArrayElement(elements)/elements.length;
+        }
+
+        private void writeLine(int result){
+            System.out.println("Answer is: " + result);
+        }
+    }
+}
